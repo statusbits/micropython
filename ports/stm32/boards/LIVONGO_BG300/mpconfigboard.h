a99edfda307b642319e67433f91be10dd27a17f2
geko <geko@statusbits.com>
2022-02-24 10:48:48 -0800

    BG300 port (work in progress)

--- ports/stm32/boards/LIVONGO_BG300/mpconfigboard.h
+++ ports/stm32/boards/LIVONGO_BG300/mpconfigboard.h
@@ -1,43 +1,45 @@
-#define MICROPY_HW_BOARD_NAME       "LIVONGO-BG300"
-#define MICROPY_HW_MCU_NAME         "STM32F401xE"
+#define MICROPY_HW_BOARD_NAME   "LIVONGO-BG300"
+#define MICROPY_HW_MCU_NAME     "STM32F401xE"
 
-#define MICROPY_HW_HAS_SWITCH       (1)
-#define MICROPY_HW_HAS_FLASH        (1)
-#define MICROPY_HW_ENABLE_RTC       (1)
-#define MICROPY_HW_ENABLE_SERVO     (1)
+#define MICROPY_HW_HAS_SWITCH   (1)
+#define MICROPY_HW_HAS_FLASH    (1)
+#define MICROPY_HW_ENABLE_RTC   (1)
+#define MICROPY_HW_ENABLE_SERVO (1)
 
 // HSE is 8MHz, HSI is 16MHz CPU freq set to 84MHz
 // Default source for the clock is HSI.
 // For revisions of the board greater than C-01, HSE can be used as a
 // clock source by removing the #define MICROPY_HW_CLK_USE_HSE line
-#define MICROPY_HW_CLK_USE_HSI (1)
+#define MICROPY_HW_CLK_USE_HSI  (1)
 
 #if MICROPY_HW_CLK_USE_HSI
-#define MICROPY_HW_CLK_PLLM (16)
+#define MICROPY_HW_CLK_PLLM     (16)
 #else
-#define MICROPY_HW_CLK_PLLM (8)
+#define MICROPY_HW_CLK_PLLM     (8)
 #endif
-#define MICROPY_HW_CLK_PLLN (336)
-#define MICROPY_HW_CLK_PLLP (RCC_PLLP_DIV4)
-#define MICROPY_HW_CLK_PLLQ (7)
+#define MICROPY_HW_CLK_PLLN     (336)
+#define MICROPY_HW_CLK_PLLP     (RCC_PLLP_DIV4)
+#define MICROPY_HW_CLK_PLLQ     (7)
 
 // UART config
-#define MICROPY_HW_UART2_TX     (pin_A2)
-#define MICROPY_HW_UART2_RX     (pin_A3)
+#define MICROPY_HW_UART1_TX     (pin_A10)
+#define MICROPY_HW_UART1_RX     (pin_A9)
+//#define MICROPY_HW_UART2_TX     (pin_D5)
+//#define MICROPY_HW_UART2_RX     (pin_D6)
 #define MICROPY_HW_UART6_TX     (pin_C6)
 #define MICROPY_HW_UART6_RX     (pin_C7)
-// UART 2 connects to the STM32F103 (STLINK) on the Nucleo board
-// and this is exposed as a USB Serial port.
-#define MICROPY_HW_UART_REPL        PYB_UART_2
+
+// UART 1 is routed to the BG300 Debug header
+#define MICROPY_HW_UART_REPL        PYB_UART_1
 #define MICROPY_HW_UART_REPL_BAUD   115200
 
 // I2C buses
-#define MICROPY_HW_I2C1_SCL (pin_B8)        // Arduino D15, pin 3 on CN10
-#define MICROPY_HW_I2C1_SDA (pin_B9)        //         D14, pin 5 on CN10
-#define MICROPY_HW_I2C2_SCL (pin_B10)       // Arduino D6,  pin 25 on CN10
-#define MICROPY_HW_I2C2_SDA (pin_B3)        // Arduino D3,  pin 31 on CN10
-#define MICROPY_HW_I2C3_SCL (pin_A8)        // Arduino D7,  pin 23 on CN10
-#define MICROPY_HW_I2C3_SDA (pin_C9)        //              pin  1 on CN10
+#define MICROPY_HW_I2C1_SCL     (pin_B8)    // Arduino D15, pin 3 on CN10
+#define MICROPY_HW_I2C1_SDA     (pin_B9)    //         D14, pin 5 on CN10
+#define MICROPY_HW_I2C2_SCL     (pin_B10)   // Arduino D6,  pin 25 on CN10
+#define MICROPY_HW_I2C2_SDA     (pin_B3)    // Arduino D3,  pin 31 on CN10
+#define MICROPY_HW_I2C3_SCL     (pin_A8)    // Arduino D7,  pin 23 on CN10
+#define MICROPY_HW_I2C3_SDA     (pin_C9)    //              pin  1 on CN10
 
 // SPI buses
 #define MICROPY_HW_SPI1_NSS     (pin_A15)   //              pin 17 on CN7
@@ -62,6 +64,6 @@
 #define MICROPY_HW_USRSW_PRESSED    (0)
 
 // LEDs
-#define MICROPY_HW_LED1             (pin_A5) // Green LD2 LED on Nucleo
-#define MICROPY_HW_LED_ON(pin)      (mp_hal_pin_high(pin))
-#define MICROPY_HW_LED_OFF(pin)     (mp_hal_pin_low(pin))
+#define MICROPY_HW_LED1         (pin_A5) // Green LD2 LED on Nucleo
+#define MICROPY_HW_LED_ON(pin)  (mp_hal_pin_high(pin))
+#define MICROPY_HW_LED_OFF(pin) (mp_hal_pin_low(pin))
